37ee01e102a1ed7e809810f76ac55f2334c0ea92
Pawel <p.czernic98@gmail.com>
2022-10-05 02:33:25 +0200

    added basic feature

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-import urllib.request
 import pandas as pd
+import numpy as np
+from classes import Satellite
 
 pd.set_option('display.max_rows', None)
 
 almanac_path = r"http://celestrak.org/GPS/almanac/Yuma/2022/almanac.yuma.week0182.405504.txt"
+almanac_data = pd.read_csv(almanac_path, delimiter=':', names=['name', 'value'])
+# print(almanac_data)
 
-almanac = urllib.request.urlopen(almanac_path)
-almanac_data = pd.read_csv(almanac_path)
+sat_index_df = almanac_data[almanac_data['value'].isna()]
+sat_index = sat_index_df.index.tolist()
 
-#
-# df = pd.DataFrame(almanac_data)
-# print(df.loc[[0]])
+sats = []
+for i in sat_index:
+    sats.append(Satellite(almanac_data.iat[i+1, 1], almanac_data.iat[i+2, 1], almanac_data.iat[i+3, 1],
+                          almanac_data.iat[i+4, 1], almanac_data.iat[i+5, 1], almanac_data.iat[i+6, 1],
+                          almanac_data.iat[i+7, 1], almanac_data.iat[i+8, 1], almanac_data.iat[i+9, 1],
+                          almanac_data.iat[i+10, 1], almanac_data.iat[i+11, 1], almanac_data.iat[i+12, 1]))
+
+sats[1].print_info()
